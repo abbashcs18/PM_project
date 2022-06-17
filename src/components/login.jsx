2e3a71655b82e8923d0abe58cde9b404ad474a91
Abhishek Rathi <abhish.cs18@nish.ac.in>
2022-06-17 10:28:12 +0530

    change color login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,25 +1,21 @@
 import React from 'react'
 
-
-
-
 const Login = () => {
   return (
     <div class="relative flex flex-col justify-center min-h-screen overflow-hidden">
-    <div class="w-full p-6 m-auto bg-white rounded shadow-lg ring-2 ring-purple-800/50 lg:max-w-md">
-        <h1 class="text-3xl font-semibold text-center text-purple-700">LOGO</h1>
+    <div class="w-full p-6 m-auto bg-white rounded shadow-lg ring-2 ring-purple-800/400 lg:max-w-md">
+        <h1 class="text-3xl font-semibold text-center text-orange-400 ">LOGO</h1>
 
         <form class="mt-6">
             <div>
-                <label for="email" class="block text-sm text-gray-800">Email</label>
-                <input type="email"
-                    class="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                <input type="email" 
+                    class="block w-full px-4 py-2 mt-2 text-purple-700  bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40 placeholder-red-300 border border-red-400" placeholder="Email "/>
             </div>
             <div class="mt-4">
                 <div>
-                    <label for="password" class="block text-sm text-gray-800">Password</label>
+                    
                     <input type="password"
-                        class="block w-full px-4 py-2 mt-2 text-orange-500 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
+                        class="block w-full px-4 py-2 mt-2 text-purple-500 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40 placeholder-red-300 border border-red-400" placeholder="Password"/>
                 </div>
                
                 <div class="mt-6">
