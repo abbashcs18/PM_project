2b208945a3c2b49870c9c2163748ae88410622e3
ABBAS HABBEEB <abbash.cs18@nish.domain>
2022-06-17 09:37:13 +0530

    deleted images and renew code for Login with tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Login from './components/Login';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import IN from './components/IN';
+
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <div>
       <Navbar/>
       {/* <Home/> */}
+      <Login/>
     </div>
   );
 }
